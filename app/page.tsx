b5180543cf0b82e3365227fc0633d422ae8d3e75
Richard Imaoka <richard.s.imaoka@gmail.com>
2026-06-14 04:38:47 +0900

    firebase ui init

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,95 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+// Import the functions you need from the SDKs you need
+import { initializeUI } from "@firebase-oss/ui-core";
+import { FirebaseUIProvider } from "@firebase-oss/ui-react";
+import "@firebase-oss/ui-styles/dist.min.css";
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhRkgQY9dNuBkwQre4SYm3wG4BDLo8_R8",
+  authDomain: "richard-playground-382018.firebaseapp.com",
+  projectId: "richard-playground-382018",
+  storageBucket: "richard-playground-382018.firebasestorage.app",
+  messagingSenderId: "248858822102",
+  appId: "1:248858822102:web:b2668a54ea781616ed5077",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const ui = initializeUI({
+  app,
+});
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
+    <FirebaseUIProvider ui={ui}>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js logo"
+            width={100}
+            height={20}
+            priority
+          />
+          <div className={styles.intro}>
+            <h1>To get started, edit the page.tsx file.</h1>
+            <p>
+              Looking for a starting point or more instructions? Head over to{" "}
+              <a
+                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Templates
+              </a>{" "}
+              or the{" "}
+              <a
+                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learning
+              </a>{" "}
+              center.
+            </p>
+          </div>
+          <div className={styles.ctas}>
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className={styles.primary}
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
+              <Image
+                className={styles.logo}
+                src="/vercel.svg"
+                alt="Vercel logomark"
+                width={16}
+                height={16}
+              />
+              Deploy Now
+            </a>
             <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className={styles.secondary}
+              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+              Documentation
+            </a>
+          </div>
+        </main>
+      </div>
+    </FirebaseUIProvider>
   );
 }
